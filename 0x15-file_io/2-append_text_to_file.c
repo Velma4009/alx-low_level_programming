22c8c57acd9965ae36e6d839fef6190b22cc736a
Velma4009 <velmaatieno1996@gmail.com>
2023-10-30 12:45:51 -0700

    file projects done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,13 +20,13 @@ int _strlen(char *s)
 
 
 /**
- * create_file - creates a file
+ * append_text_to_file - appends a text at the end of a file
  * @filename: name of file
  * @text_content: text
  * Return: 1 - success, -1 error
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	unsigned int length;
@@ -34,17 +34,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (text_content == NULL)
+		return (1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-
-	if (text_content == NULL)
-	{
-		length = 0;
-		text_content = "";
-	}
-	else
-		length = _strlen(text_content);
+	length = _strlen(text_content);
 
 	bytes_written = write(fd, text_content, length);
 	if (bytes_written == -1)
